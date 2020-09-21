12171e5a3842c9cf91e3a6b5bcadb96cf401ab4d
Christian Spinelli <christian.spinelli@hibo.local>
2020-09-21 11:30:02 +0200

    fix win unix

--- src/Console.php
+++ src/Console.php
@@ -10,18 +10,18 @@ class Console{
 	 * @param boolean $default
 	 * @param array $yes = ['yes','y','si']
 	 * @param array $no = ['no','n']
-	 * @return int 
+	 * @return int
 	 */
 	public static function yesNo(string $prompt, bool $default=false, array $yes = ['yes','y','si'], array $no = ['no','n']){
 		while(true){
-			$resp = readline($prompt." (yes, no)? [".(($default)?'yes':'no')."] ");	
-			if($resp==NULL || $resp==''){			
+			$resp = self::readByOs($prompt." (yes, no)? [".(($default)?'yes':'no')."] ");
+			if($resp==NULL || $resp==''){
 				return $default;
 			}else{
 				if(in_array($resp,$yes)) return true;
-				if(in_array($resp,$no)) return false;					
+				if(in_array($resp,$no)) return false;
 			}
-		}		
+		}
 	}
 
 	/**
@@ -43,29 +43,37 @@ class Console{
 	 * @param array $options = []
 	 * @return void
 	 */
-	public static function read(string $prompt, $default=NULL, array $options=[]){
-		if(count($options)>0){
-			self::write($prompt);
-			foreach ($options as $key => $value) {
-				($key === $value)? self::write($value.', ',false) : self::write('    '.$key . ') ' . $value);
-			}
-			while(true){
-				$sel = readline("Chose an option from listed above: [".$default."] ");
-				if($sel===NULL || $sel === '') return $default;
-				if(key_exists($sel,$options)) return $options[$sel];	
-			}
-		}else{
-			while(true){
-				$resp = readline($prompt.(($default!==NULL)? ": [".$default."] " : ": "));			
+	public static function read(string $prompt, $default = NULL, array $options = []){
+		if(empty($options)){
+      while(true){
+				$resp = self::readByOs($prompt.(($default!==NULL)? ": [".$default."] " : ": "));
 				if($resp === NULL || $resp === ''){
 					if($default !== NULL ) return $default;
 				}else{
 					return $resp;
 				}
 			}
+		}else{
+			self::write($prompt);
+			foreach ($options as $key => $value) {
+				($key === $value)? self::write($value.', ',false) : self::write('    '.$key . ') ' . $value);
+			}
+			while(true){
+				$sel = self::readByOs("Chose an option from listed above: [".$default."] ");
+				if($sel===NULL || $sel === '') return $default;
+				if(key_exists($sel,$options)) return $options[$sel];
+			}
 		}
-	}
+  }
+
+
+  private static function readByOs(string $prompt){
+    echo "$prompt";
+    if (PHP_OS == 'WINNT') {
+      return stream_get_line(STDIN, 1024, PHP_EOL);
+    } else {
+      return readline($prompt);
+    }
+  }
 
 }
-
-?>
--- test/test.php
+++ test/test.php
@@ -1,9 +1,23 @@
+#!/bin/php
 <?php
 
-require_once(__DIR__.'/../vendor/autoload.php');
+include_once __DIR__ . '/../vendor/autoload.php';
 
-use Chr1sp1n\Console;
+use Hibo\Console;
 
-var_dump( Console::yesNo("Yes or Not") );
+$t =  Console::read('Simple read test');
+var_dump($t);
+echo PHP_EOL;
 
-?>
+$t =  Console::read('Simple read test', 1, ['1' => 'Option 1', '2' => 'Option 2', '3' => 'Option 3']);
+var_dump($t);
+echo PHP_EOL;
+
+
+$t =  Console::yesNo('Yes,no test');
+var_dump($t);
+echo PHP_EOL;
+
+
+
+
